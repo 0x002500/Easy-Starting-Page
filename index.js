23fdcf6e122168f491eb8cb13728125842801124
0x002500 <tiger_pan@outlook.com>
2024-03-17 18:00:26 +0800

    [feat] make the defult theme to dark mode

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const buttons = {
-    google: document.getElementById('search-google'),
-    bing: document.getElementById('search-bing'),
-    duckduckgo: document.getElementById('search-duckduckgo'),
-    baidu: document.getElementById('search-baidu')
+    google: document.getElementById("search-google"),
+    bing: document.getElementById("search-bing"),
+    duckduckgo: document.getElementById("search-duckduckgo"),
+    baidu: document.getElementById("search-baidu"),
 };
 
 const engines = {
-    google: 'https://www.google.com/search?q=',
-    bing: 'https://www.bing.com/search?q=',
-    duckduckgo: 'https://duckduckgo.com/?q=',
-    baidu: 'https://www.baidu.com/s?wd='
+    google: "https://www.google.com/search?q=",
+    bing: "https://www.bing.com/search?q=",
+    duckduckgo: "https://duckduckgo.com/?q=",
+    baidu: "https://www.baidu.com/s?wd=",
 };
 
-Object.keys(buttons).forEach(key => {
-    buttons[key].addEventListener('click', function () {
-        var query = document.getElementById('search-bar').value;
+Object.keys(buttons).forEach((key) => {
+    buttons[key].addEventListener("click", function () {
+        var query = document.getElementById("search-bar").value;
         if (query) {
             window.open(engines[key] + query);
         }
@@ -22,5 +22,12 @@ Object.keys(buttons).forEach(key => {
 });
 
 document.getElementById("toggleTheme").onclick = function () {
-    document.body.classList.toggle('dark-theme');
+    var body = document.body;
+    if (body.classList.contains("dark-theme")) {
+        body.classList.remove("dark-theme");
+        body.classList.add("light-theme");
+    } else {
+        body.classList.remove("light-theme");
+        body.classList.add("dark-theme");
+    }
 };
